95684329c9bf9cd0331306c4a6e289501f6e1add
mouhcine <mouhcine15.00@gmail.com>
2022-08-24 18:08:08 +0200

    methods e axios

--- public/js/front.js
+++ public/js/front.js
@@ -5283,8 +5283,32 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+/* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_0__);
+
 /* harmony default export */ __webpack_exports__["default"] = ({
-  name: 'PageContacts'
+  name: 'PageContacts',
+  data: function data() {
+    return {
+      name: '',
+      mail: '',
+      message: '',
+      mailinglist: true
+    };
+  },
+  methods: {
+    submitMessage: function submitMessage() {
+      console.log('funziona');
+      axios.post('/aoi/leads/create', {
+        name: this.name,
+        email: this.email,
+        message: this.message,
+        mailingList: this.mailingList
+      }).then(function (res) {
+        return console.log(res.data);
+      });
+    }
+  }
 });
 
 /***/ }),
@@ -5657,14 +5681,14 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _vm._m(0);
-};
-
-var staticRenderFns = [function () {
-  var _vm = this,
-      _c = _vm._self._c;
-
-  return _c("div", [_c("h1", [_vm._v("Contact us")]), _vm._v(" "), _c("form", [_c("div", {
+  return _c("div", [_c("h1", [_vm._v("Contact us")]), _vm._v(" "), _c("form", {
+    on: {
+      submit: function submit($event) {
+        $event.preventDefault();
+        return _vm.submitMessage.apply(null, arguments);
+      }
+    }
+  }, [_c("div", {
     staticClass: "mb-3"
   }, [_c("label", {
     staticClass: "form-label",
@@ -5672,11 +5696,26 @@ var staticRenderFns = [function () {
       "for": "name"
     }
   }, [_vm._v("Name")]), _vm._v(" "), _c("input", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.name,
+      expression: "name"
+    }],
     staticClass: "form-control",
     attrs: {
       type: "text",
       name: "name",
       id: "name"
+    },
+    domProps: {
+      value: _vm.name
+    },
+    on: {
+      input: function input($event) {
+        if ($event.target.composing) return;
+        _vm.name = $event.target.value;
+      }
     }
   })]), _vm._v(" "), _c("div", {
     staticClass: "mb-3"
@@ -5686,11 +5725,26 @@ var staticRenderFns = [function () {
       "for": "email"
     }
   }, [_vm._v("email")]), _vm._v(" "), _c("input", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.email,
+      expression: "email"
+    }],
     staticClass: "form-control",
     attrs: {
       type: "text",
       name: "email",
       id: "email"
+    },
+    domProps: {
+      value: _vm.email
+    },
+    on: {
+      input: function input($event) {
+        if ($event.target.composing) return;
+        _vm.email = $event.target.value;
+      }
     }
   })]), _vm._v(" "), _c("div", {
     staticClass: "mb-3"
@@ -5700,20 +5754,63 @@ var staticRenderFns = [function () {
       "for": "message"
     }
   }, [_vm._v("message")]), _vm._v(" "), _c("textarea", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.message,
+      expression: "message"
+    }],
     staticClass: "form-control",
     attrs: {
       name: "message",
       id: "message",
       cols: "30",
       rows: "10"
+    },
+    domProps: {
+      value: _vm.message
+    },
+    on: {
+      input: function input($event) {
+        if ($event.target.composing) return;
+        _vm.message = $event.target.value;
+      }
     }
   })]), _vm._v(" "), _c("div", [_c("input", {
+    directives: [{
+      name: "model",
+      rawName: "v-model",
+      value: _vm.mailinglist,
+      expression: "mailinglist"
+    }],
     staticClass: "form-check-input",
     attrs: {
       type: "checkbox",
       name: "newsletter",
-      id: "newsletter",
-      checked: ""
+      id: "newsletter"
+    },
+    domProps: {
+      checked: Array.isArray(_vm.mailinglist) ? _vm._i(_vm.mailinglist, null) > -1 : _vm.mailinglist
+    },
+    on: {
+      change: function change($event) {
+        var $$a = _vm.mailinglist,
+            $$el = $event.target,
+            $$c = $$el.checked ? true : false;
+
+        if (Array.isArray($$a)) {
+          var $$v = null,
+              $$i = _vm._i($$a, $$v);
+
+          if ($$el.checked) {
+            $$i < 0 && (_vm.mailinglist = $$a.concat([$$v]));
+          } else {
+            $$i > -1 && (_vm.mailinglist = $$a.slice(0, $$i).concat($$a.slice($$i + 1)));
+          }
+        } else {
+          _vm.mailinglist = $$c;
+        }
+      }
     }
   }), _vm._v(" "), _c("label", {
     staticClass: "form-check-label",
@@ -5726,7 +5823,9 @@ var staticRenderFns = [function () {
       type: "submit"
     }
   }, [_vm._v("Send")])])]);
-}];
+};
+
+var staticRenderFns = [];
 render._withStripped = true;
 
 
--- resources/js/pages/PageContacts.vue
+++ resources/js/pages/PageContacts.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <h1>Contact us</h1>
-    <form>
+    <form @submit.prevent="submitMessage">
         <div class="mb-3">
             <label class="form-label" for="name">Name</label>
-            <input class="form-control" type="text" name="name" id="name"> 
+            <input class="form-control" type="text" name="name" id="name" v-model="name"> 
         </div>
         <div class="mb-3">
             <label class="form-label" for="email">email</label>
-            <input class="form-control" type="text" name="email" id="email"> 
+            <input class="form-control" type="text" name="email" id="email" v-model="email"> 
         </div>
         <div class="mb-3">
             <label class="form-label" for="message">message</label>
-            <textarea class="form-control" name="message" id="message" cols="30" rows="10"></textarea>
+            <textarea class="form-control" name="message" id="message" cols="30" rows="10" v-model="message"></textarea>
         </div>
         <div>
-          <input class="form-check-input" type="checkbox" name="newsletter" id="newsletter" checked>
+          <input class="form-check-input" type="checkbox" name="newsletter" id="newsletter" v-model="mailinglist">
           <label class="form-check-label" for="newsletter">Iscrivimi alla newsletter</label>
         </div>
         <button type="submit" class="btn btn-primary">Send</button>
@@ -24,8 +24,30 @@
 </template>
 
 <script>
+import Axios from 'axios';
+
 export default {
     name: 'PageContacts',
+    data() {
+      return {
+        name: '',
+        mail: '',
+        message: '',
+        mailinglist: true,
+      }
+    },
+    methods: {
+      submitMessage() {
+        console.log('funziona');
+        axios.post('/aoi/leads/create', {
+          name: this.name,
+          email: this.email,
+          message: this.message,
+          mailingList: this.mailingList
+        })
+         .then(res => console.log(res.data));
+      }
+    }
 }
 </script>
 
